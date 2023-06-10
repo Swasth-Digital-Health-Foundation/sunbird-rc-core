490f3d96d4424368cdf41b3e4d641ac89cc95578
shiva-rakshith <rakshiths@sanketika.in>
2023-06-10 20:11:55 +0530

    Revert "feat: update authorization filter"
    This reverts commit 051de6b79ed551ac560de0d17031f24a57eba2b2.

--- java/registry/src/main/java/dev/sunbirdrc/registry/config/SecurityConfig.java
+++ java/registry/src/main/java/dev/sunbirdrc/registry/config/SecurityConfig.java
@@ -21,9 +21,6 @@ import org.springframework.security.web.authentication.session.RegisterSessionAu
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 //@Configuration
-//@EnableWebSecurity
-//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-//@ConditionalOnProperty(name = "authentication.enabled",havingValue = "true",matchIfMissing = false)
 //public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 //
 //    @Value("${authentication.enabled:true}") boolean authenticationEnabled;
